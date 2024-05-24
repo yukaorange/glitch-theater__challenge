dd7bd6869a813ac760a15248fd5a3b07094b554f
yukaorange <takaoka.webdesigner@gmail.com>
2024-05-24 16:52:09 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Experience } from "@/components/Experience";
+import { FC } from "react";
 
-function App() {
+const App: FC = () => {
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
   );
-}
+};
 
 export default App;
